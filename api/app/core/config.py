0f3efa90c3d5e89f38dffa883e3dab39875405fe
zhenia-cyp <kviat.zhenia@gmail.com>
2025-11-19 10:47:02 +0200

    add postgresql database as service with healthcheck to docker compose

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -8,6 +8,12 @@ class Settings(BaseSettings):
     DEBUG: bool = True
     RELOAD: bool = True
 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
